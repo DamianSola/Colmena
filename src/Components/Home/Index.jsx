56cd60e09f8f16389b7a3e6eda6daa351822d29b
damian sola <damiansola99@gmail.com>
2024-02-14 12:37:06 -0300

    listooo

--- src/Components/Home/Index.jsx
+++ src/Components/Home/Index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <h1 className="text-6xl md:text-8xl py-2 tracking-widest">Colmena</h1>
             <h4 className="text-base md:text-4xl py-1 tracking-widest">Espacio + Comunidad</h4>
         </div>
-        <div className="flex-col sm:flex my-18 text-center justify-center pb-10">
+        <div className="flex flex-wrap my-18 text-center justify-center pb-10">
             <Link className="text-white w-52 hover:text-orange hover:bg-white hover:border-white border-2 px-10 py-2 mx-5 my-5 font-medium" href="#about">Nosotros</Link>
             <Link className="text-white w-52 hover:text-orange hover:bg-white hover:border-white border-2 px-10 py-2 mx-5 my-5 font-medium" href="#projects">Proyectos</Link>
         </div>
